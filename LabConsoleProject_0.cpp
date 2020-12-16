8a5d07df07dd23a6c87908bb7074d16b88def051
MorningBlueOfficial <MorningBlueOfficial@users.noreply.github.com>
2020-12-16 15:32:37 +0200

    LengX engine prototype
    listing functionality

--- LabConsoleProject_0.cpp
+++ LabConsoleProject_0.cpp
@@ -8,6 +8,15 @@
 #include <fstream>
 #include <sstream>
 
+
+// load words: read file (user inputs file name) or make user to type new word (user inputs word manually)
+//      check if new words are already in the library, if new words are in library check their Hard-To-Remember-Index (HRI)
+
+// suggest 15 random words for repeating and recalling
+// prompt to value how hard was to remember a word ( 0 - easy to remember, 10 - can't remember at all)
+//      adjust words HRI index accordingly to result
+
+
 int main()
 {
      return 0;
